071127f100953af4ef039735cfe87e74772e8f98
Dimitri.Yatsenko@gmail.com <Dimitri.Yatsenko@gmail.com@2f38a6b3-5502-11bb-9305-b9e7ad8f4d38>
2012-02-09 17:30:47 +0000

    disabled reconnects in dj.conn
    git-svn-id: http://datajoint.googlecode.com/svn/trunk/matlab@458 2f38a6b3-5502-11bb-9305-b9e7ad8f4d38

--- +dj/conn.m
+++ +dj/conn.m
@@ -5,30 +5,23 @@ function connObj = conn(host, user, pass, initQuery)
 % call dj.conn to get the connection object.
 %
 % The first time dj.conn is called, it must establish a conection. The
-% connection parameters may be specified by input arguments. Any omitted 
-% paramters are taken from the environment variables DJ_HOST, DJ_USER, DJ_PASS, 
-% and DJ_INIT are used. Finally, if the required parameters are still missing, 
+% connection parameters may be specified by input arguments. Any omitted
+% paramters are taken from the environment variables DJ_HOST, DJ_USER, DJ_PASS,
+% and DJ_INIT are used. Finally, if the required parameters are still missing,
 % the user is prompted to enter them manually.
 %
-% The last parameter, initQuery (or the environemnt variable) DJ_INIT specify 
-% the query to be executed everytime a new connection session is established. 
+% The last parameter, initQuery (or the environemnt variable) DJ_INIT specify
+% the query to be executed everytime a new connection session is established.
 %
-% Once established during the first invocation, the connection object cannot 
+% Once established during the first invocation, the connection object cannot
 % be changed. To reset the connection, use 'clear functions' or 'clear classes'.
 
 persistent CONN_OBJ
 
-% disconnect if the host or the user have changed
-if nargin>0 && isa(CONN_OBJ, 'dj.Connection')
-    if ~strcmp(host, CONN_OBJ.host) || ~strcmp(user, CONN_OBJ.user)
-        CONN_OBJ.close
-    end
-    if ~CONN_OBJ.isConnected
-        CONN_OBJ = '';
-    end
-end
-
-if isempty(CONN_OBJ)
+if isa(CONN_OBJ, 'dj.Connection')
+    assert(nargin==0, ...
+        'connection already instantiated. To reconnect, clear functions')
+else
     % optional environment variables specifying the connection.
     env  = struct(...
         'host', 'DJ_HOST', ...
