aa709d33645eb8beea806a8065dff64cda179366
dimitri <dimitri.yatsenko@gmail.com>
2012-08-18 23:01:59 -0500

    minor cleanup

--- +dj/new.m
+++ +dj/new.m
@@ -2,14 +2,20 @@ function new(className)
 % DJ.NEW - interactively create a new DataJoint table/class
 %
 % INPUT:
-%   className in the format "package.ClassName"
+%   className in the format 'package.ClassName'
 
 if nargin<1
     className = input('Enter <package>.<ClassName>: ','s');
 end
 
 p = find(className == '.', 1, 'last');
-assert(~isempty(p), 'dj.new: specify package.ClassName"')
+if isempty(p)
+    throwAsCaller(MException('DataJoint:makeClass', 'dj.new requires package.ClassName"'))
+end
 
-schemaObj = eval([className(1:p-1) '.getSchema']);
-schemaObj.makeClass(className(p+1:end))
+schemaFunction = [className(1:p-1) '.getSchema'];
+if isempty(which(schemaFunction))
+    throwAsCaller(MException('DataJoint:makeClass', 'Cannot find %s', schemaFunction))
+end
+
+makeClass(eval(schemaFunction), className(p+1:end))
