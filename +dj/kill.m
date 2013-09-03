3d545717e303db8b0f148fdb2d1b6cd3f6ddbaf5
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-09-03 10:44:29 +0200

    dj.kill: Added option to restrict connections that are shown and to kill all connections at once

--- +dj/kill.m
+++ +dj/kill.m
@@ -1,10 +1,23 @@
 % dj.kill - show MySQL all connections and prompt to kill a connection.
 
-function kill
+function kill(restriction)
+
+qstr = 'SELECT * FROM information_schema.processlist WHERE id <> CONNECTION_ID()';
+if nargin && ~isempty(restriction)
+    qstr = sprintf('%s AND %s', qstr, restriction);
+end
+    
 while true
-    query(dj.conn, 'show processlist')
-    id = input('process to kill (''q''-quit) >','s');
-    if ischar(id) && strncmpi(id,'q',1)
+    query(dj.conn, qstr)
+    id = input('process to kill (''q''-quit, ''a''-all) > ', 's');
+    if ischar(id) && strncmpi(id, 'q', 1)
+        break
+    elseif ischar(id) && strncmpi(id, 'a', 1)
+        res = query(dj.conn, qstr);
+        id = double(res.ID)';
+        for i = id
+            query(dj.conn, 'kill {Si}', i)
+        end
         break
     end
     id = sscanf(id,'%d');
