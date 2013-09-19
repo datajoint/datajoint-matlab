datajoint/datajoint-matlab

commit 4cba4bd3d7e9df9cdcaff8259f8d6f55a16b6753

prohibited nulls for integer attributes.  A few cosmetic changes.

+dj/Schema.m
@@ -522,7 +522,7 @@ classdef Schema < handle
                     'SELECT table_name AS `table`, column_name as `name`,'...
                     '(column_key="PRI") AS `iskey`,column_type as `type`,'...
                     '(is_nullable="YES") AS isnullable, column_comment as `comment`,'...
-                    'if(is_nullable="YES","NULL",ifnull(CAST(column_default AS CHAR),"<<<none>>>"))  AS `default` '...
+                    'if(is_nullable="YES","NULL",ifnull(CAST(column_default AS CHAR),"<<<no default>>>"))  AS `default` '...
                     'FROM information_schema.columns '...
                     'WHERE table_schema="%s" and table_name REGEXP "{S}"'],...
                     self.dbname),self.tableRegexp);
@@ -536,7 +536,7 @@ classdef Schema < handle
                     '^(tiny|medium|long)?blob', 'once')), self.header.type);
                 % strip field lengths off integer types
                 self.header.type = cellfun(@(x) regexprep(char(x'), ...
-                    '((tiny|long|small|)int)\(\d+\)','$1'), self.header.type, 'UniformOutput', false);
+                    '((tiny|small|medium|big)?int)\(\d+\)','$1'), self.header.type, 'UniformOutput', false);
                 self.header.alias = repmat({''}, length(self.header.name),1);
                 self.header = dj.struct.fromFields(self.header);
                 self.header = self.header(ismember({self.header.table}, {self.tables.name}));

+dj/Table.m
@@ -26,7 +26,7 @@ classdef (Sealed) Table < handle
     properties(Dependent, SetAccess = private)
         info     % name, tier, comment.  See dj.Schema
         header    % structure array describing header
-        fullTableName  % table name with database, escaped in backquotes
+        fullTableName  % `database`.`plain_table_name`
         plainTableName  % just the table name
     end
     
@@ -58,16 +58,11 @@ classdef (Sealed) Table < handle
             if isempty(self.schema)
                 schemaFunction = regexprep(self.className, '\.\w+$', '.getSchema');
                 assert(~isempty(which(schemaFunction)), ['Not found: ' schemaFunction])
-                self.schema = eval(schemaFunction);
+                self.schema = feval(schemaFunction);
                 assert(isa(self.schema, 'dj.Schema'), ...
                     [schemaFunction ' must return an instance of dj.Schema'])
             end
             ret = self.schema;
-        end
-        
-        
-        function yes = exists(self)
-            yes = any(strcmp(self.className, self.schema.classNames));
         end
         
         
@@ -206,7 +201,7 @@ classdef (Sealed) Table < handle
                 
                 if self.header(i).isnullable
                     default = '=null';
-                elseif strcmp(char(self.header(i).default(:)'), '<<<none>>>')
+                elseif strcmp(char(self.header(i).default(:)'), '<<<no default>>>')
                     default = '';
                 elseif self.header(i).isNumeric || ...
                         any(strcmp(self.header(i).default,self.mysql_constants))
@@ -548,6 +543,11 @@ classdef (Sealed) Table < handle
     
     methods(Access=private)
         
+        function yes = exists(self)
+            yes = any(strcmp(self.className, self.schema.classNames));
+        end
+
+        
         function neighbors = getNeighbors(self, depth1, depth2, crossSchemas)
             % dj.Table/getNeighbors -- get the class names of tables that are
             % directly related to the given table.
@@ -794,7 +794,6 @@ end
 if ischar(l)
     while true
         l = fgetl(f);
-        assert(ischar(l), 'invalid verbatim string');
         if strcmp(strtrim(l),'%}')
             break
         end
@@ -815,7 +814,7 @@ default = field.default;
 if strcmpi(default, 'NULL')   % all nullable attributes default to null
     default = 'DEFAULT NULL';
 else
-    if strcmp(default,'<<<none>>>')  %DataJoint's special value string
+    if strcmp(default,'<<<no default>>>')  %DataJoint's special value to indicate no default
         default = 'NOT NULL';
     else
         % enclose value in quotes (even numeric), except special SQL values
@@ -909,10 +908,15 @@ fieldInfo = regexp(line, cat(2,pat{:}), 'names');
 if isempty(fieldInfo)
     % try no default value
     fieldInfo = regexp(line, cat(2,pat{[1 3 4]}), 'names');
-    assert(~isempty(fieldInfo), 'invalid field declaration line: %s', line);
-    fieldInfo.default = '<<<none>>>';
+    assert(~isempty(fieldInfo), 'invalid field declaration line: %s', line)
+    fieldInfo.default = '<<<no default>>>';  % special value indicating no default
 end
 assert(numel(fieldInfo)==1, 'Invalid field declaration "%s"', line)
+if ~isempty(regexp(fieldInfo.datatype,'^(tiny|small|medium|big)?int', 'once')) ...
+        && strcmp(fieldInfo.default,'null')
+    throw(MException('DataJoint:invalidDeclaration', ...
+        'Integer attributes cannot be nullable in "%s"', line))
+end
 fieldInfo.iskey = inKey;
 end
 

+dj/GeneralRelvar.m
@@ -33,15 +33,15 @@ classdef GeneralRelvar < matlab.mixin.Copyable
         end
         
         function header = get.header(self)
-            header = self.compile();
+            header = self.compile;
         end
         
         function s = get.sql(self)
-            [~, s] = self.compile();
+            [~, s] = self.compile;
         end
         
         function schema = get.schema(self)
-            schema = self.getSchema();
+            schema = self.getSchema;
         end
         
         function names = get.primaryKey(self)
@@ -81,9 +81,7 @@ classdef GeneralRelvar < matlab.mixin.Copyable
                 % print header
                 header = self.header;
                 ix = find( ~[header.isBlob] );  % header to display
-                fprintf \n
-                fprintf('  %16.16s', header(ix).name)
-                fprintf \n
+                fprintf('\n  %16.16s\n', header(ix).name)
                 maxRows = 12;
                 tuples = self.fetch(header(ix).name, sprintf('LIMIT %d', maxRows+1));
                 nTuples = max(self.count, length(tuples));
@@ -102,13 +100,13 @@ classdef GeneralRelvar < matlab.mixin.Copyable
                             fprintf('  %16.16s',v)
                         end
                     end
-                    fprintf '\n'
+                    fprintf \n
                 end
                 if nTuples > maxRows
                     for iField = ix
                         fprintf('  %16s','...')
                     end
-                    fprintf '\n'
+                    fprintf \n
                 end
             end
             
@@ -354,9 +352,9 @@ classdef GeneralRelvar < matlab.mixin.Copyable
             % expression.
             %
             % Examples:
-            %   Scans & struct('mouse_id',3, 'scannum', 4);
-            %   Scans & 'lens=10'
-            %   Mice & (Scans & 'lens=10')
+            %   tp.Scans & struct('mouse_id',3, 'scannum', 4);
+            %   tp.Scans & 'lens=10'
+            %   tp.Mice & (tp.Scans & 'lens=10')
             if ~iscell(arg)
                 arg = {arg};
             end
@@ -533,11 +531,11 @@ classdef GeneralRelvar < matlab.mixin.Copyable
         
         function schema = getSchema(self)
             % get reference to the schema from the first base relvar
-            schema = self.operands{1};
+            op = self.operands{1};
             if strcmp(self.operator, 'table')
-                schema = schema.schema;
+                schema = op.schema;
             else
-                schema = schema.getSchema();
+                schema = op.getSchema;
             end
         end
         
@@ -884,8 +882,7 @@ end
 
 function str = escapeString(str)
 % Escapes strings that are used in SQL clauses by struct2cond.
-% We use ' to enclose strings, so we need to replace all instances
-% of ' with ''.
+% We use ' to enclose strings, so we need to replace all instances of ' with ''.
 % To prevent the expansion of MySQL escape characters, all instances
 % of \ have to be replaced with \\.
 str = strrep(str, '''', '''''');

commit 4d8512eddaa72c5fc6256ae8e44a59729da06cd2

made dj.Schema/reload compatible with a future fix for mym's issue #2. Fixed a table declaration parsing bug introduced in 4cba4bd.

+dj/Schema.m
@@ -528,14 +528,14 @@ classdef Schema < handle
                     self.dbname),self.tableRegexp);
                 self.header.isnullable = logical(self.header.isnullable);
                 self.header.iskey = logical(self.header.iskey);
-                self.header.isNumeric = ~cellfun(@(x) isempty(regexp(char(x'), ...
+                self.header.isNumeric = ~cellfun(@(x) isempty(regexp(sprintf('%s',x), ...
                     '^((tiny|small|medium|big)?int|decimal|double|float)', 'once')), self.header.type);
-                self.header.isString = ~cellfun(@(x) isempty(regexp(char(x'), ...
+                self.header.isString = ~cellfun(@(x) isempty(regexp(sprintf('%s',x), ...
                     '^((var)?char|enum|date|time|timestamp)','once')), self.header.type);
-                self.header.isBlob = ~cellfun(@(x) isempty(regexp(char(x'), ...
+                self.header.isBlob = ~cellfun(@(x) isempty(regexp(sprintf('%s',x), ...
                     '^(tiny|medium|long)?blob', 'once')), self.header.type);
                 % strip field lengths off integer types
-                self.header.type = cellfun(@(x) regexprep(char(x'), ...
+                self.header.type = cellfun(@(x) regexprep(sprintf('%s',x), ...
                     '((tiny|small|medium|big)?int)\(\d+\)','$1'), self.header.type, 'UniformOutput', false);
                 self.header.alias = repmat({''}, length(self.header.name),1);
                 self.header = dj.struct.fromFields(self.header);
@@ -548,8 +548,6 @@ classdef Schema < handle
                 end
                 fprintf('%.3g\n',toc)
             end
-            
-            
         end
         
         
@@ -621,8 +619,7 @@ classdef Schema < handle
             if ix
                 db = [db '/' tab(1:ix(1)-1)];
                 tab = tab(ix(1)+1:end);
-            end
-            
+            end            
             str = self.conn.getPackage(['$' db '.' dj.Schema.toCamelCase(tab)]);
         end
     end

+dj/Table.m
@@ -913,7 +913,7 @@ if isempty(fieldInfo)
     fieldInfo.default = '<<<no default>>>';  % special value indicating no default
 end
 assert(numel(fieldInfo)==1, 'Invalid field declaration "%s"', line)
-if ~isempty(regexp(fieldInfo.datatype,'^(tiny|small|medium|big)?int', 'once')) ...
+if ~isempty(regexp(fieldInfo.type,'^(tiny|small|medium|big)?int', 'once')) ...
         && strcmp(fieldInfo.default,'null')
     throw(MException('DataJoint:invalidDeclaration', ...
         'Integer attributes cannot be nullable in "%s"', line))

+dj/version.m
@@ -1,7 +1,7 @@
 function varargout = version
 % report DataJoint version
 
-v = struct('major',2,'minor',6,'bugfix',3);
+v = struct('major',2,'minor',6,'bugfix',4);
 
 if nargout
     varargout{1}=v;