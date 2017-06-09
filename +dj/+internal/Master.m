91880769f9049d30bd259fa14dd2df88e83d1e29
Edgar Y. Walker <edgar.walker@gmail.com>
2017-06-09 00:39:23 -0500

    Bug fix on startsWith usage

--- +dj/+internal/Master.m
+++ +dj/+internal/Master.m
@@ -7,7 +7,7 @@ classdef Master < handle
             % find classnames that begin with me and are dj.Part
             info = meta.class.fromName(class(self));
             classNames = {info.ContainingPackage.ClassList.Name};
-            list = cellfun(@feval, classNames(startsWith(classNames, class(self))), 'uni', false);
+            list = cellfun(@feval, classNames(dj.lib.startsWith(classNames, class(self))), 'uni', false);
             list = list(cellfun(@(x) isa(x, 'dj.Part'), list));
         end
     end
