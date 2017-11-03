7ef34723333ff498023cad5a02bf3a4663cd22de
Edgar Y. Walker <edgar.walker@gmail.com>
2017-11-03 13:31:54 -0500

    Minor enhancement to erd

--- erd.m
+++ erd.m
@@ -12,19 +12,20 @@ ret = dj.ERD();
 for entity = varargin
     if exist(entity{1}, 'class')
         obj = dj.ERD(feval(entity{1}));
-        r = dj.set('tableErdRadius');
-        while min(r)>0
-            if r(1)>0
-                obj.up
-                r(1) = r(1)-1;
-            end
-            if r(2)>0
-                obj.down
-                r(2) = r(2)-1;
-            end
-        end
+        
     else
         obj = dj.ERD(feval([entity{1} '.getSchema']));
+    end
+    r = dj.set('tableErdRadius');
+    while max(r)>0
+        if r(1)>0
+            obj.up
+            r(1) = r(1)-1;
+        end
+        if r(2)>0
+            obj.down
+            r(2) = r(2)-1;
+        end
     end
     ret = ret + obj;
 end
