780fc45db5ee067feb3b8ae93feef25bd02069bc
Edgar Y. Walker <edgar.walker@gmail.com>
2019-02-05 22:26:59 -0600

    Increment version number

--- +dj/version.m
+++ +dj/version.m
@@ -1,7 +1,7 @@
 function varargout = version
 % report DataJoint version
 
-v = struct('major',3,'minor',2,'bugfix',0);
+v = struct('major',3,'minor',2,'bugfix',2);
 
 if nargout
     varargout{1}=v;
