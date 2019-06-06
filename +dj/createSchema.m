746da1f6d2d6e9617ee1b666d69f3c21874b0be9
guzman-raphael <raphael.h.guzman@gmail.com>
2019-06-06 10:03:51 -0500

    Add ability to create schema with no GUI.

--- +dj/createSchema.m
+++ +dj/createSchema.m
@@ -1,12 +1,16 @@
-function createSchema(package)
+function createSchema(package,parentdir,db)
 % DJ.CREATESCHEMA - interactively create a new DataJoint schema
 %
 % INPUT:
-%   (optional) package - name of the package to be associated with the
-%   schema
+%   (optional) package - name of the package to be associated with the schema
+%   (optional) parentdir - name of the dirctory where to create new package
+%   (optional) db - database name to associate with the schema
 
-
-dbname = input('Enter database name >> ','s');
+if nargin < 3
+    dbname = input('Enter database name >> ','s');
+else
+    dbname = db;
+end
 
 if ~dbname
     disp 'No database name entered. Quitting.'
@@ -16,26 +20,39 @@ else
     % create database
     s = query(dj.conn, ...
         sprintf('SELECT schema_name FROM information_schema.schemata WHERE schema_name = "%s"', dbname));
-    
+
     if ~isempty(s.schema_name)
         disp 'database already exists'
     else
         query(dj.conn, sprintf('create schema %s',dbname))
         disp 'database created'
     end
-    
+
     if nargin < 1
-        disp 'Please select a package folder. Opening UI...'
-        folder = uigetdir('./','Select a package folder');
+        if usejava('desktop')
+            disp 'Please select a package folder. Opening UI...'
+            folder = uigetdir('./','Select a package folder');
+        else
+            folder = input('Enter package folder path >> ','s');
+        end
     else
-        fprintf('Please select folder to create package %s in. Opening UI...\n', ['+', package])
-        folder = uigetdir('./', sprintf('Select folder to create package %s in', ['+', package]));
+        if nargin < 3
+            if usejava('desktop')
+                fprintf('Please select folder to create package %s in. Opening UI...\n', ['+', package])
+                folder = uigetdir('./', sprintf('Select folder to create package %s in', ['+', package]));
+            else
+                folder = input('Enter parent folder path >> ','s');
+            end
+        else
+            folder = parentdir;
+        end
+
         if folder
             folder = fullfile(folder, ['+', package]);
             mkdir(folder)
         end
     end
-    
+
     if ~folder
         disp 'No package selected.  Cancelled.'
     else
@@ -44,7 +61,7 @@ else
             error 'Package folders must start with a +'
         end
         package = package(2:end);  % discard +
-        
+
         % create the getSchema function
         schemaFile = fullfile(folder,'getSchema.m');
         if exist(schemaFile,'file')
@@ -52,7 +69,7 @@ else
         else
             f = fopen(schemaFile,'wt');
             assert(-1 ~= f, 'Could not open %s', f)
-            
+
             fprintf(f,'function obj = getSchema\n');
             fprintf(f,'persistent schemaObject\n');
             fprintf(f,'if isempty(schemaObject)\n');
@@ -62,7 +79,7 @@ else
             fprintf(f,'end\n');
             fclose(f);
         end
-        
+
         % test that getSchema is on the path
         whichpath = which(sprintf('%s.getSchema',package));
         if isempty(whichpath)
