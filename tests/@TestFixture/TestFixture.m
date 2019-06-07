4b83769cdf1e7a18cb7d6dea0b6608f34aa60163
guzman-raphael <raphael.h.guzman@gmail.com>
2019-06-07 15:36:30 -0500

    Supress warning + fix lint.

--- tests/@TestFixture/TestFixture.m
+++ tests/@TestFixture/TestFixture.m
@@ -1,11 +1,15 @@
 classdef TestFixture < matlab.unittest.TestCase
     
     properties (Constant)
-        CONN_INFO = struct('host', getenv('DJ_TEST_HOST'), 'user', getenv('DJ_TEST_USER'), 'password', getenv('DJ_TEST_PASSWORD'));
+        CONN_INFO = struct(...
+            'host', getenv('DJ_TEST_HOST'), ...
+            'user', getenv('DJ_TEST_USER'), ...
+            'password', getenv('DJ_TEST_PASSWORD'));
     end
 
     methods (TestClassSetup)
         function init(testCase)
+            disp('---------------INIT---------------');
             addpath('..');
             testCase.addTeardown(@testCase.dispose);
         end
@@ -17,7 +21,13 @@ classdef TestFixture < matlab.unittest.TestCase
     
     methods (Static)
         function dispose()
+            disp('---------------DISP---------------');
+            warning('off','MATLAB:RMDIR:RemovedFromPath');
+            
             clear functions;
+            rmdir('../mym', 's');
+
+            warning('on','MATLAB:RMDIR:RemovedFromPath');
         end
     end
 end
