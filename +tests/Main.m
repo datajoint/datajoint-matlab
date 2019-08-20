0d53777cfdc9ae141f382ad8946ea8aefb7cfc4f
guzman-raphael <raphael.h.guzman@gmail.com>
2019-08-20 11:18:16 -0500

    Fix incorrect indention.

--- +tests/Main.m
+++ +tests/Main.m
@@ -1,7 +1,7 @@
 classdef Main < ...
     tests.TestConnection
 
-     properties (Constant)
+    properties (Constant)
         CONN_INFO_ROOT = struct(...
             'host', getenv('DJ_HOST'), ...
             'user', getenv('DJ_USER'), ...
@@ -12,16 +12,16 @@ classdef Main < ...
             'password', getenv('DJ_TEST_PASSWORD'));
     end
 
-     methods (TestClassSetup)
+    methods (TestClassSetup)
         function init(testCase)
             disp('---------------INIT---------------');
             clear functions;
             testCase.addTeardown(@testCase.dispose);
 
-             curr_conn = dj.conn(testCase.CONN_INFO_ROOT.host, ...
+            curr_conn = dj.conn(testCase.CONN_INFO_ROOT.host, ...
                 testCase.CONN_INFO_ROOT.user, testCase.CONN_INFO_ROOT.password,'',true);
 
-             ver = curr_conn.query('select @@version as version').version;
+            ver = curr_conn.query('select @@version as version').version;
             if dj.lib.compareVersions(ver,'5.8')
                 cmd = {...
                 'CREATE USER IF NOT EXISTS ''datajoint''@''%%'' '
@@ -29,30 +29,30 @@ classdef Main < ...
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
 
-                 cmd = {...
+                cmd = {...
                 'GRANT ALL PRIVILEGES ON `djtest%%`.* TO ''datajoint''@''%%'';'
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
 
-                 cmd = {...
+                cmd = {...
                 'CREATE USER IF NOT EXISTS ''djview''@''%%'' '
                 'IDENTIFIED BY ''djview'';'
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
 
-                 cmd = {...
+                cmd = {...
                 'GRANT SELECT ON `djtest%%`.* TO ''djview''@''%%'';'
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
 
-                 cmd = {...
+                cmd = {...
                 'CREATE USER IF NOT EXISTS ''djssl''@''%%'' '
                 'IDENTIFIED BY ''djssl'' '
                 'REQUIRE SSL;'
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
 
-                 cmd = {...
+                cmd = {...
                 'GRANT SELECT ON `djtest%%`.* TO ''djssl''@''%%'';'
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
@@ -63,13 +63,13 @@ classdef Main < ...
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
 
-                 cmd = {...
+                cmd = {...
                 'GRANT SELECT ON `djtest%%`.* TO ''djview''@''%%'' '
                 'IDENTIFIED BY ''djview'';'
                 };
                 curr_conn.query(sprintf('%s',cmd{:}));
 
-                 cmd = {...
+                cmd = {...
                 'GRANT SELECT ON `djtest%%`.* TO ''djssl''@''%%'' '
                 'IDENTIFIED BY ''djssl'' '
                 'REQUIRE SSL;'
@@ -79,15 +79,15 @@ classdef Main < ...
         end
     end
 
-     methods (Static)
+    methods (Static)
         function dispose()
             disp('---------------DISP---------------');
             warning('off','MATLAB:RMDIR:RemovedFromPath');
 
-             curr_conn = dj.conn(tests.Main.CONN_INFO_ROOT.host, ...
+            curr_conn = dj.conn(tests.Main.CONN_INFO_ROOT.host, ...
                 tests.Main.CONN_INFO_ROOT.user, tests.Main.CONN_INFO_ROOT.password, '',true);
 
-             cmd = {...
+            cmd = {...
             'DROP USER ''datajoint''@''%%'';'
             'DROP USER ''djview''@''%%'';'
             'DROP USER ''djssl''@''%%'';'
@@ -95,7 +95,7 @@ classdef Main < ...
             res = curr_conn.query(sprintf('%s',cmd{:}));
             curr_conn.delete;
 
-             warning('on','MATLAB:RMDIR:RemovedFromPath');
+            warning('on','MATLAB:RMDIR:RemovedFromPath');
         end
     end
 end
