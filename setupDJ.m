a42397beb525f45f75b45772b775e430653e3408
Chris Turner <cat@199tech.net>
2020-06-26 11:42:26 -0500

    setupDJ.m: use new setupMYM

--- setupDJ.m
+++ setupDJ.m
@@ -25,27 +25,7 @@ function setupDJ(skipPathAddition, force)
         addpath(base)
     end
 
-    mymdir = fullfile(base, 'mym');
-    % if mym directory missing, download and install
-    if ~isdir(mymdir)
-        fprintf('mym missing. Downloading...\n')
-        target = fullfile(base, 'mym.zip');
-        mymURL = 'https://github.com/datajoint/mym/archive/master.zip';
-        target = websave(target, mymURL);
-        if isunix && ~ismac
-            % on Linux Matlab unzip doesn't work properly so use system unzip
-            system(sprintf('unzip -o %s -d %s', target, base))
-        else
-            unzip(target, base)
-        end
-        % rename extracted mym-master directory to mym
-        movefile(fullfile(base, 'mym-master'), mymdir)
-        delete(target)
-    end
-    
-    % run mymSetup.m
-    fprintf('Setting up mym...\n')
-    run(fullfile(mymdir, 'mymSetup.m'))
+    setupMYM('master', force);
 
     try
         mymVersion = mym('version');
