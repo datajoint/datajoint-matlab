602be47e90cbd3c169f398fd32ad90fc10efab26
Chris Turner <cat@199tech.net>
2020-08-06 18:26:41 -0500

    += +tests/TestSetupMYM.m skeleton

---
+++ +tests/TestSetupMYM.m
@@ -0,0 +1,28 @@
+
+classdef TestSetupMYM < tests.Prep
+% TestSetupMyM tests setupMYM
+
+    methods (Test)
+        function TestSetupMYM_testDefaultInstall(testCase)
+
+        end
+        function TestSetupMYM_testVersionInstallFresh(testCase)
+
+        end
+        function TestSetupMYM_testVersionInstallStale(testCase)
+
+        end
+        function TestSetupMYM_testVersionInstallStaleForce(testCase)
+
+        end
+        function TestSetupMYM_testMasterInstallFresh(testCase)
+
+        end
+        function TestSetupMYM_testMasterInstallStale(testCase)
+
+        end
+        function TestSetupMYM_testMasterInstallStaleForce(testCase)
+
+        end
+    end
+end
