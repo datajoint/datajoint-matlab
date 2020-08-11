887aa198cf7305322e3800dfa3c2c2691955c2ad
guzman-raphael <raphael.h.guzman@gmail.com>
2020-08-11 16:19:05 -0500

    Update tests and update virtual classes to be compatible with reverse engineering.

--- tests/TestSchema.m
+++ tests/TestSchema.m
@@ -2,6 +2,8 @@ classdef TestSchema < Prep
     % TestSchema tests related to schemas.
     methods (Test)
         % https://github.com/datajoint/datajoint-matlab/issues/254
+        % external table included in schema.tableNames
+        % external table included in schema.classNames
         function TestSchema_testUnsupportedDJTypes(testCase)
             st = dbstack;
             disp(['---------------' st(1).name '---------------']);
@@ -19,10 +21,7 @@ classdef TestSchema < Prep
             dj.config('stores',struct(store_name, struct('protocol', 'file', 'location', ...
                 store_dir)));
             id = 2;
-            % schema = External.getSchema;
-            schema = dj.Schema(c1, package, [testCase.PREFIX '_' lower(package)]);
-            schema.v
-            q = schema.v.Document & ['document_id=' num2str(id)]
+            External.Document
             c1.query(['insert into `' testCase.PREFIX '_' lower(package) '`.`~external_' ...
                 store_name '`(hash,size,attachment_name,filepath,contents_hash) values ' ...
                 '(X''1d751e2e1e74faf84ab485fde8ef72be'', 1, ''attach_name'', ''filepath'',' ...
@@ -33,17 +32,27 @@ classdef TestSchema < Prep
                 'values (' num2str(id) ', ''raphael'', ''hello'',' ...
                 'X''1d751e2e1e74faf84ab485fde8ef72be'',' ...
                 'X''1d751e2e1e74faf84ab485fde8ef72bf'')']);
-            % display test
-            q
-            % test reverse engineering
+            delete([testCase.test_root '/test_schemas/+' package '/getSchema.m']);
+            dj.createSchema(package,[testCase.test_root '/test_schemas'], ...
+                [testCase.PREFIX '_' lower(package)]);
+            schema = External.getSchema;
+            q = schema.v.Document & ['document_id=' num2str(id)];
+            % display
+            queryPreview = evalc("q");
+            queryPreview = splitlines(queryPreview);
+            recordPreview = queryPreview(end-4);
+            recordPreview = strsplit(recordPreview{1});
+            testCase.verifyTrue(all(cellfun(@(x) strcmp(x,'''=BLOB='''), ...
+                recordPreview(4:end-1))));
+            % reverse engineering
             raw_def = dj.internal.Declare.getDefinition(q);
             assembled_def = describe(q);
             [raw_sql, ~] = dj.internal.Declare.declare(q, raw_def);
             [assembled_sql, ~] = dj.internal.Declare.declare(q, assembled_def);
             testCase.verifyEqual(raw_sql,  assembled_sql);
-            % test fetch good
+            % fetch good
             testCase.verifyEqual(q.fetch1('document_id'),  id);
-            % test fetch bad
+            % fetch bad
             for c = ["document_data1", "document_data2", "document_data3"]
                 try
                     res = q.fetch(char(c));
@@ -53,7 +62,7 @@ classdef TestSchema < Prep
                     end
                 end
             end
-            % test insert bad
+            % insert bad
             try
                 insert(q, struct( ...
                     'document_id', 3, ...
@@ -67,11 +76,11 @@ classdef TestSchema < Prep
                     rethrow(ME);
                 end
             end
-            % test update good
+            % update good
             new_name = 'peter';
             update(q, 'document_name', new_name);
             testCase.verifyEqual(q.fetch1('document_name'),  new_name);
-            % test update bad
+            % update bad
             for c = ["document_data1", "document_data2", "document_data3"]
                 try
                     update(q, char(c), 'this');
@@ -84,53 +93,6 @@ classdef TestSchema < Prep
             % clean up
             rmdir(store_dir);
             dj.config.restore;
-            % display
-            % v
-            % external table included in schema.tableNames
-            % external table included in schema.classNames
         end
-        % function TestSchema_testUnsupportedDJTypes(testCase)
-        %     st = dbstack;
-        %     disp(['---------------' st(1).name '---------------']);
-        %     package = 'University';
-
-        %     c1 = dj.conn(...
-        %         testCase.CONN_INFO.host,... 
-        %         testCase.CONN_INFO.user,...
-        %         testCase.CONN_INFO.password,'',true);
-
-        %     dj.createSchema(package,[testCase.test_root '/test_schemas'], ...
-        %         [testCase.PREFIX '_university']);
-
-        %     University.Message
-        %     c1.query('ALTER TABLE `djtest_university`.`message` CHANGE `dep_id` `dep_id` binary(16) COMMENT '':attach:''');
-        %     delete([testCase.test_root '/test_schemas/+' package '/getSchema.m']);
-
-        %     dj.createSchema(package,[testCase.test_root '/test_schemas'], ...
-        %         [testCase.PREFIX '_university']);
-        %     University.getSchema;
-        %     % in progress...
-        %     % test display
-        %     University.Message
-        %     % test reverse engineering
-        %     % q = University.Message;
-        %     % raw_def = dj.internal.Declare.getDefinition(q);
-        %     % assembled_def = describe(q);
-        %     % [raw_sql, ~] = dj.internal.Declare.declare(q, raw_def);
-        %     % [assembled_sql, ~] = dj.internal.Declare.declare(q, assembled_def);
-        %     % testCase.verifyEqual(raw_sql,  assembled_sql);
-        %     % test fetch good
-        %     q = University.Message;
-        %     res = q.fetch('msg_id');
-        %     % test fetch bad
-        %     % res = q.fetch('dep_id');
-        %     % test insert
-        %     % insert(University.Message, struct( ...
-        %     %     'msg_id', '1d751e2e-1e74-faf8-4ab4-85fde8ef72be', ...
-        %     %     'body', 'Great campus!', ...
-        %     %     'dep_id', 'ter' ...
-        %     % ));
-        %     % % test update
-        % end
     end
 end
