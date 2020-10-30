77738abffee54127dbff3ce61fcf657e3cf37fca
Chris Turner <cat@199tech.net>
2020-10-29 22:23:42 -0500

    +dj/kill.m: case-insensitize id field in query results, reindent+end statement

--- +dj/kill.m
+++ +dj/kill.m
@@ -26,45 +26,54 @@
 
 function kill(restriction, connection, order_by)
 
-if nargin < 3
-    order_by = {};
-end
+    if nargin < 3
+        order_by = {};
+    end
 
-if nargin < 2
-    connection = dj.conn;
-end
+    if nargin < 2
+        connection = dj.conn;
+    end
 
-qstr = 'SELECT * FROM information_schema.processlist WHERE id <> CONNECTION_ID()';
+    qstr = 'SELECT * FROM information_schema.processlist WHERE id <> CONNECTION_ID()';
 
-if nargin && ~isempty(restriction)
-    qstr = sprintf('%s AND (%s)', qstr, restriction);
-end
+    if nargin && ~isempty(restriction)
+        qstr = sprintf('%s AND (%s)', qstr, restriction);
+    end
 
-if isempty(order_by)
-    qstr = sprintf('%s ORDER BY id', qstr);
-else
-    if iscell(order_by)
-        qstr = sprintf('%s ORDER BY %s', qstr, strjoin(order_by, ','));
+    if isempty(order_by)
+        qstr = sprintf('%s ORDER BY id', qstr);
     else
-        qstr = sprintf('%s ORDER BY %s', qstr, order_by);
-    end
-end
-
-while true
-    query(connection, qstr)
-    id = input('process to kill (''q''-quit, ''a''-all) > ', 's');
-    if ischar(id) && strncmpi(id, 'q', 1)
-        break
-    elseif ischar(id) && strncmpi(id, 'a', 1)
-        res = query(connection, qstr);
-        id = double(res.ID)';
-        for i = id
-            query(connection, 'kill {Si}', i)
+        if iscell(order_by)
+            qstr = sprintf('%s ORDER BY %s', qstr, strjoin(order_by, ','));
+        else
+            qstr = sprintf('%s ORDER BY %s', qstr, order_by);
         end
-        break
     end
-    id = sscanf(id,'%d');
-    if ~isempty(id) 
-        query(connection, 'kill {Si}', id(1))
+
+    while true
+        query(connection, qstr)
+        id = input('process to kill (''q''-quit, ''a''-all) > ', 's');
+        if ischar(id) && strncmpi(id, 'q', 1)
+            break
+        elseif ischar(id) && strncmpi(id, 'a', 1)
+            res = query(connection, qstr);
+
+            if isfield(res, 'ID')  % MySQL >8.x variation
+                id_field = 'ID';
+            else
+                id_field = 'id';
+            end
+
+            id = double(res.(id_field))';
+            for i = id
+                query(connection, 'kill {Si}', i)
+            end
+            break
+        end
+        id = sscanf(id,'%d');
+        if ~isempty(id) 
+            query(connection, 'kill {Si}', id(1))
+        end
     end
+
 end
