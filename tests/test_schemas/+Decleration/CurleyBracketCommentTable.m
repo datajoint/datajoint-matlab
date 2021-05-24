f2ec9f1f3febc7626f9cfa5ea3e55e2397862457
Synicix <synicix@gmail.com>
2021-05-23 23:11:26 -0500

    Table for decleration test

---
+++ tests/test_schemas/+Decleration/CurleyBracketCommentTable.m
@@ -0,0 +1,9 @@
+%{
+# subject information
+subject_fullname            : varchar(64)                   # {username}_{subject_nickname}
+---
+subject_nickname            : varchar(16)                   # nickname
+%}
+
+classdef CurleyBracketCommentTable < dj.Manual
+end
