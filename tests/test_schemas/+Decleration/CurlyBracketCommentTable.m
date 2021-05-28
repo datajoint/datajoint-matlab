4a0cfa3b5e55a3c42309047aec5cfb271306268f
Synicix <synicix@gmail.com>
2021-05-28 11:28:45 -0500

    Update comment

--- tests/test_schemas/+Decleration/CurlyBracketCommentTable.m
+++ tests/test_schemas/+Decleration/CurlyBracketCommentTable.m
@@ -1,8 +1,8 @@
 %{
 # subject information
-curly_bracket_comment_table_subject_fullname            : varchar(64)                   # {username}_{subject_nickname}
+curly_bracket_comment_table_subject_fullname    : varchar(64)   # {username}_{subject_nickname}
 ---
-curly_bracket_comment_table_subject_nickname            : varchar(16)                   # nickname
+curly_bracket_comment_table_subject_nickname    : varchar(16)   # nickname
 %}
 
 classdef CurlyBracketCommentTable < dj.Manual
