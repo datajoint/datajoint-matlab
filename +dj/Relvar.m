daee5fdfc17555a1215e6b38542e0104292d5301
“Carlos <carlosortiz9204@gmail.com>
2022-02-16 12:22:38 -0600

    Staging for debugging

--- +dj/Relvar.m
+++ +dj/Relvar.m
@@ -92,15 +92,17 @@ classdef Relvar < dj.internal.GeneralRelvar & dj.internal.Table
                                 @(x) strcmp(x.from, rels(ix).fullTableName), ...
                                 self.schema.conn.foreignKeys, 'uni', true);
                             fks = self.schema.conn.foreignKeys(fk_index);
+%                             fprintf '\nWhat is ref_attrs and attrs:'
+%                             fks.ref_attrs
+%                             fks.attrs
+%                             if isequal(fks.aliased, 0)
                             if ~fks.aliased
                                 % If matched foreign keys are not aliased, no renaming
                                 % necessary. Restrict table based on normal projection.
                                 rels(ix).restrict(proj(rels(i)));
                             else
                                 % Determine which foreign keys have been renamed
-                                alias_index = cellfun(...
-                                    @(ref_attr, attr) ~strcmp(ref_attr, attr), ...
-                                    fks.ref_attrs, fks.attrs, 'uni', true);
+                                alias_index = cellfun(@(ref_attr, attr) ~strcmp(ref_attr, attr), fks.ref_attrs, fks.attrs, 'uni', true);
                                 % Create rename arguments for projection
                                 aliased_attrs = cellfun(...
                                     @(ref_attr, attr) sprintf('%s->%s', ref_attr, attr), ...
---
+++ tests/test_schemas/+TestLabDif/Duty.m
@@ -0,0 +1,8 @@
+%{
+duty_date:  date
+----- 
+-> TestLabDif.User
+%}
+
+classdef Duty < dj.Manual
+end
